6bccd2767a1050dbaf9183e1d0da45460d5acb82
Maryus Martsyalis <martsyalis@gmail.com>
2021-09-13 23:15:19 -0600

    lambda mongo

--- netlify/functions/tweeting-api/index.js
+++ netlify/functions/tweeting-api/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
-const {connection, Random} = require('./connection');
+const { connection, Random } = require("./connection");
 
-
-app.use(express.json());
-app.use("/", async (req, res) => {
+exports.handler = async event => {
     await connection;
-    console.log("tweed data is: ", req.body);
-    const newTweet = await Random.create(req.body);
-    res.json(newTweet);
-});
-
-exports.handler = serverless(app);
+    console.log("event is: ", event);
+    switch (event.httpMethod) {
+        case "POST": {
+            const newRandom = await Random.create(JSON.parse(event.body));
+            return {
+                statusCode: 201,
+                body: `new entry with id of: ${newRandom._id} was created`,
+            };
+        }
+        case "GET": {
+            const allRandom = await Random.find();
+            return {
+                statusCode: 200,
+                body: JSON.stringify(allRandom),
+            };
+        }
+    }
+};
