e1331fdeab55734499bbd0ada6be72beb8b4745d
Maryus Martsyalis <martsyalis@gmail.com>
2021-09-13 23:25:18 -0600

    refactor routes

--- netlify/functions/tweeting-api/index.js
+++ netlify/functions/tweeting-api/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
-const {connection, Random} = require('./connection');
+const morgan = require("morgan");
+const { connection, Random } = require("./connection");
 
-
+app.use(morgan("tiny"));
 app.use(express.json());
-app.use("/", async (req, res) => {
+
+// this long path is super annoyiing but nesseserry. you can aslo do "*" to catch all posts
+app.post("/.netlify/functions/tweeting-api/", async (req, res) => {
+    console.log("in post: ", req);
     await connection;
     console.log("tweed data is: ", req.body);
     const newTweet = await Random.create(req.body);
